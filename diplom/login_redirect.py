7facc52bef36bf7c263f9b0a1115c7dc1753f9f4
Daniar-git <daniar.78@inbox.ru>
2024-05-17 12:41:19 +0500

    fix middleware

--- diplom/login_redirect.py
+++ diplom/login_redirect.py
@@ -1,18 +1,12 @@
+# myapp/middleware.py
 from django.shortcuts import redirect
-from django.urls import reverse
 
-
-class CustomRedirectMiddleware:
+class LoginRedirectMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-
-        if request.user.is_authenticated:
-            login_url = "https://diplom.aitu.lol/login/"
-            if request.path == login_url:
-                next_url = 'https://diplom.aitu.lol'
-                return redirect(next_url)
-
+        if request.user.is_authenticated and 'next' in request.GET:
+            return redirect("https://diplom.aitu.lol/")
         return response
