9b0fd842965964a9e9c085d7e2e086eb7ca567d2
Daniar-git <daniar.78@inbox.ru>
2024-06-02 16:36:47 +0500

    New commit message yesterday

--- diplom/user/urls.py
+++ diplom/user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from diplom.user.views import profile, error_page
 
 urlpatterns = [
-    path('profile/', profile, name="profile"),#user/profile/
+    path('profile/', profile, name="profile"),
     path('error/', error_page, name="error_page"),
 ]
